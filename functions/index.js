9704aac4c3cad3d5f86feb2d39afa1ae577db2ad
Andrzejka <a.andrzejewska96@gmail.com>
2024-12-19 11:54:15 +0100

    Configure Firebase Hosting

--- functions/index.js
+++ functions/index.js
@@ -13,45 +13,61 @@ const logger = require("firebase-functions/logger");
 const admin = require('firebase-admin');
 
 admin.initializeApp();
-console.log({admin});
 
 https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = onRequest((request, response) => {
+exports.helloWorld = onRequest( async(request, response) => {
   logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello bjdbscdisbsfugwe");
-});
+  // admin.firestore().collection('users').doc().set({email: 'hvvh'}), 
+  admin.firestore().collection('users').doc("CzlOTwYoGPJiVKe5Tbop").delete();
+  response.send("Hello bjdbscdisbsfugwe"+JSON.stringify(request.body));
 
-exports.createUser = onCall(async (data, context) => {
-  // Check if the request is authenticated
-  console.log(context.auth);
-  // if (!context.auth) {
-  //   throw new functions.https.HttpsError(
-  //     "unauthenticated",
-  //     "The function must be called while authenticated."
-  //   );
-  // }
-
-  const { email, password, displayName } = data;
-
+  let aa;
   try {
-    // Create a new user
-    const userRecord = await admin.auth().createUser({
-      email,
-      password,
-      displayName,
-    });
-    console.log({ userRecord });
-    return {
-      message: "User created successfully",
-      uid: userRecord.uid,
-    };
-  } catch (error) {
-    throw new functions.https.HttpsError(
-      "internal",
-      error.message || "Failed to create user"
-    );
+    aa = await admin
+      .firestore()
+      .collection("users")
+      .where("email", "==", "test")
+      .get();
+  } catch {
+    response.send("not working");
   }
+  logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello bjdbscdisbsfugwe" + JSON.stringify(aa));
+
 });
+// CzlOTwYoGPJiVKe5Tbop
+// admin.firestore().collection('users').doc().set({email: 'hvvh'}),
+// exports.createUser = onCall(async (data, context) => {
+//   // Check if the request is authenticated
+//   console.log(context.auth);
+//   // if (!context.auth) {
+//   //   throw new functions.https.HttpsError(
+//   //     "unauthenticated",
+//   //     "The function must be called while authenticated."
+//   //   );
+//   // }
+
+//   const { email, password, displayName } = data;
+
+//   try {
+//     // Create a new user
+//     const userRecord = await admin.auth().createUser({
+//       email,
+//       password,
+//       displayName,
+//     });
+//     console.log({ userRecord });
+//     return {
+//       message: "User created successfully",
+//       uid: userRecord.uid,
+//     };
+//   } catch (error) {
+//     throw new functions.https.HttpsError(
+//       "internal",
+//       error.message || "Failed to create user"
+//     );
+//   }
+// });
 
 
