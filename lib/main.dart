ccaa75a6e1ad14aa72d14f1c256a6bffd42ad669
Andrzejka <a.andrzejewska96@gmail.com>
2024-12-28 21:13:05 +0100

    Update firebase-hosting file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flexchamp/app/core/injection_container.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart'; 
@@ -5,6 +6,7 @@ import 'app/app.dart';
 
 void main() async {
 WidgetsFlutterBinding.ensureInitialized();
+configureDependencies();
 await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,
 ); 
