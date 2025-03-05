049191d94a9fd9d6f7404d711faca97737804856
Andrzejka <a.andrzejewska96@gmail.com>
2025-03-05 22:46:38 +0100

    Add dio handling errors

--- lib/data/affirmation_remote_data_source.dart
+++ lib/data/affirmation_remote_data_source.dart
@@ -5,8 +5,12 @@ class AffirmationRemoteDataSource {
   Future<Map<String, dynamic>?> getAffirmationData({
     required String imageUrl,
   }) async {
-    final response = await Dio().get<Map<String, dynamic>>(
+    try {
+      final response = await Dio().get<Map<String, dynamic>>(
         'https://andrzejka-dev.github.io/Affirmations/affirmations.json');
     return response.data;
+    } on DioException catch (error) {
+      throw Exception(error.response?.data['error']['message'] ?? ['Unknown error']);
+    }
   }
 }
